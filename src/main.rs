0711a0f40292b84a565c70376e4ea7a7f31760da
pfcoder <pfcoder97@gmail.com>
2020-02-02 20:13:08 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate serde_derive;
+
+mod cli_args;
+mod database;
+mod errors;
+mod graphql;
+mod jwt;
+mod schema;
+mod user;
+
+use actix_identity::{CookieIdentityPolicy, IdentityService};
+use actix_web::{App, HttpServer};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+  dotenv::dotenv().ok();
+
+  let opt = {
+    use structopt::StructOpt;
+    cli_args::Opt::from_args()
+  };
+
+  let schema = std::sync::Arc::new(crate::graphql::model::create_schema());
+  let domain = opt.domain.clone();
+  let cookie_secret_key = opt.auth_secret_key.clone();
+  let secure_cookie = opt.secure_cookie;
+  let auth_duration = chrono::Duration::hours(i64::from(opt.auth_duration_in_hour));
+  let port = opt.port;
+
+  let server = HttpServer::new(move || {
+    App::new()
+      .data(database::pool::establish_connection(opt.clone()))
+      .data(schema.clone())
+      .data(opt.clone())
+      .wrap(IdentityService::new(
+        CookieIdentityPolicy::new(cookie_secret_key.as_bytes())
+          .name("auth")
+          .path("/")
+          .domain(&domain)
+          .max_age_time(auth_duration)
+          .secure(secure_cookie),
+      ))
+      .configure(user::route)
+      .configure(graphql::route)
+  })
+  .bind(("0.0.0.0", port))
+  .unwrap()
+  .run();
+
+  eprintln!("Listening on 0.0.0.0:{}", port);
+
+  server.await
 }
---
+++ src/user/model.rs
@@ -0,0 +1,86 @@
+use crate::schema::*;
+use crate::user::util::{make_hash, make_salt};
+use chrono::*;
+use shrinkwraprs::Shrinkwrap;
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, Queryable, juniper::GraphQLObject)]
+pub struct User {
+  pub id: i32,
+  pub uuid: Uuid,
+  #[graphql(skip)]
+  pub hash: Vec<u8>,
+  #[graphql(skip)]
+  pub salt: String,
+  pub email: String,
+  pub role: String,
+  pub name: String,
+  pub created_at: NaiveDateTime,
+}
+
+#[derive(Debug, Insertable)]
+#[table_name = "users"]
+pub struct InsertableUser {
+  pub uuid: Uuid,
+  pub hash: Vec<u8>,
+  pub salt: String,
+  pub email: String,
+  pub created_at: NaiveDateTime,
+  pub name: String,
+  pub role: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UserData {
+  pub name: String,
+  pub email: String,
+  pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct SlimUser {
+  pub uuid: Uuid,
+  pub email: String,
+  pub role: String,
+}
+
+#[derive(Shrinkwrap, Clone, Default)]
+pub struct LoggedUser(pub Option<SlimUser>);
+
+impl From<SlimUser> for LoggedUser {
+  fn from(slim_user: SlimUser) -> Self {
+    LoggedUser(Some(slim_user))
+  }
+}
+
+impl From<UserData> for InsertableUser {
+  fn from(user_data: UserData) -> Self {
+    let UserData {
+      name,
+      email,
+      password,
+      ..
+    } = user_data;
+
+    let salt = make_salt();
+    let hash = make_hash(&password, &salt).to_vec();
+    Self {
+      uuid: Uuid::new_v4(),
+      email,
+      hash,
+      created_at: chrono::Local::now().naive_local(),
+      salt,
+      name,
+      role: "user".to_owned(),
+    }
+  }
+}
+impl From<User> for SlimUser {
+  fn from(user: User) -> Self {
+    let User {
+      uuid, email, role, ..
+    } = user;
+
+    Self { uuid, email, role }
+  }
+}
